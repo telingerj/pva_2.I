82ec477320e31e0fb6f7cf0a3998d97ea3685e62
Jan Telinger <telingerj@skola.betlemska.cz>
2026-02-13 13:59:36 +0100

    lesson 07

--- 07_objekty/zoo.py
+++ 07_objekty/zoo.py
@@ -24,9 +24,27 @@ class Zoo:
         return False
 
 
+    def rozlohaVsechVybehu(self):
+        s = 0
+        for vybeh in self.vybehy:
+            s += vybeh.rozloha
+        return s
+
+
     def pridejVybeh(self, vybeh):
+        if self.rozlohaVsechVybehu() + vybeh.rozloha > self.rozloha:
+            return False
         self.vybehy.append(vybeh)
         vybeh.pridejZoo(self)
+        return True
+
+
+    def vratJmenaVsechZvirat(self):
+        vsechnaZvirata = []
+        for vybeh in self.vybehy:
+            for zvire in vybeh.zvirata:
+                vsechnaZvirata.append(zvire.jmeno)
+        return vsechnaZvirata
 
 
 class Vybeh:
@@ -34,19 +52,56 @@ class Vybeh:
         self.nazev = nazev
         self.rozloha = rozloha
         self.zoo = None
+        self.zvirata = []
 
 
     def pridejZoo(self, zoo):
         self.zoo = zoo
 
+
+    def pridejZvire(self, zvire):
+        if len(self.zvirata) == 0:  # výběh je prázdný - přidání se vždy povede
+            self.zvirata.append(zvire)
+            zvire.pridejVybeh(self)
+            return True
+        if zvire.typ != self.zvirata[0].typ:
+            return False
+        self.zvirata.append(zvire)
+        zvire.pridejVybeh(self)
+        return True
+
+
+class Zvire:
+    def __init__(self, jmeno, hmotnost, typ, vyska):
+        self.jmeno = jmeno
+        self.hmotnost = hmotnost
+        self.typ = typ
+        self.vyska = vyska
+        self.vybeh = None
+
+
+    def pridejVybeh(self, vybeh):
+        self.vybeh = vybeh
+
+
+
 z = Zoo("Zoo Praha", "Troja", 200, 1931, 545)
-z2 = Zoo("Zoo Plzeň", "Plzeň", 150, 1954, 320)
+z2 = Zoo("Zoo Plzeň", "Plzeň", 150, 1954, 70)
 z3 = Zoo("Zoo pohádkový les", "Pohádkový les", 100, 2010, 121)
 
 v = Vybeh("sloni", 51)
 v2 = Vybeh("žirafy", 32)
 v3 = Vybeh("ptáci", 2)
+zv1 = Zvire("Pepa", 3654, "slon", 3)
+zv2 = Zvire("Franta", 3788, "slon", 3.1)
+zv3 = Zvire("Emílie", 2874, "žirafa", 8)
 
 z.pridejVybeh(v)
 z.pridejVybeh(v2)
-print(v.zoo.rokZalozeni)
+z.pridejVybeh(v3)
+
+v.pridejZvire(zv1)
+v.pridejZvire(zv2)
+v2.pridejZvire(zv3)
+
+print(z.vratJmenaVsechZvirat())
